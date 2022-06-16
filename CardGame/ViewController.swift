5c6dca12b156568d02520d3ac6ef471794923d5e
Subheksha Gautam <subhekshagautam@gmail.com>
2022-06-16 22:47:22 +1000

    *New functionality added

--- CardGame/ViewController.swift
+++ CardGame/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreBoard: UILabel!
+    
+    @IBOutlet weak var rightScoreBoard: UILabel!
+    var leftScore = 0
+    var rightScore = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
-
+    
+    
+    @IBAction func dealTapped(_ sender: Any) {
+        // randomise number
+        
+        let leftNumber = Int.random(in: 2...14)
+        let rightNumber = Int.random(in: 2...14)
+        //update image view
+        
+        leftImageView.image = UIImage(named: "card\(leftNumber)")
+        rightImageView.image = UIImage(named: "card\(rightNumber)")
+        
+        if leftNumber > rightNumber {
+            leftScore += 1
+            leftScoreBoard.text = String(leftScore)
+        }
+        else if leftNumber < rightNumber {
+            rightScore += 1
+            rightScoreBoard.text = String(rightScore)
+            
+        }
+        else {
+            
+        }
+    }
+    
 
 }
 
